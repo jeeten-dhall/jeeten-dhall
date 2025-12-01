27191162b006d715bd911e17c7f5396b171307cd
Jeetendra Dhall <jeeten.dhall@gmail.com>
2025-12-01 17:41:00 +0530

    Sample Astro Project

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://jeeten-dhall.github.io/jeeten-dhall',
+	integrations: [mdx(), sitemap()],
+});
